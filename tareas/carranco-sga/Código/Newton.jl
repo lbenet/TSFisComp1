16b1e41de293324db444dfad6e73d7f03c0e8a4a
Gabriel Alfonso Carranco-Sapiéns <32427033+carranco-sga@users.noreply.github.com>
2018-10-01 14:04:14 -0500

    Mejorada la implementación del método de Newton

--- tareas/carranco-sga/Código/Newton.jl
+++ tareas/carranco-sga/Código/Newton.jl
@@ -1,6 +1,6 @@
 module Newton
 """
-    newton(f, fprime, x0, número_iteraciones)
+    newton(f, fprime, x0, número_iteraciones, tolerancia)
 
 `newton` es una implementación unidimensional compleja del método de Newton para encontrar raíces de la función `f`.
 
@@ -15,8 +15,9 @@ Para poder utilizar la función se requieren los siguientes argumentos:
 Opcionalmente, se puede especificar lo siguiente:
 
 * `número_iteraciones`, el número de iteraciones a realizar. (Por defecto está configurado en 1000 iteraciones.)
+* `tolerancia`, la diferencia máxima entre valores sucesivos de la iteración para devolver el valor final. (Por defecto está configurada en 1e-8.)
 
-`newton` requiere que tanto `f` como `fprime` sean funciones, que `x0` sea un número y que `número_iteraciones` sea un número entero. `x0`, en particular, siempre es convertida a un número flotante para mejorar la estabilidad de tipo.
+`newton` requiere que tanto `f` como `fprime` sean funciones, que `x0` sea un número, que `número_iteraciones` sea un número entero y que `tolerancia` sea un real. `x0`, en particular, siempre es convertida a un número flotante para mejorar la estabilidad de tipo.
 
 # Ejemplos
 ```julia-repl
@@ -26,28 +27,40 @@ Opcionalmente, se puede especificar lo siguiente:
     julia> newton(x -> x^2 - 2, x -> 2*x, 1.4, 100)
     1.414213562373095
 
-    julia> newton(x -> x^2 - 2, x -> 2*x, φ, 1)
-    1.4270509831248424
+    julia> newton(x -> x^2 - 2, x -> 2*x, MathConstants.φ, 1)
+    No hay convergencia con tolerancia = 1.0e-8. diferencia = 0.19098300562505255
 
-    julia> newton(x -> x^2 - 2, x -> 2*x, φ, 10)
+    julia> newton(x -> x^2 - 2, x -> 2*x, MathConstants.φ, 10)
     1.414213562373095
 
-    julia> newton(z -> z^2 + 1, z -> 2*z, -0.1*im, 5)
+    julia> newton(z -> z^2 + 1, z -> 2*z, -0.1*im, 5, 1e-1)
     0.0 - 1.0032578510960606im
 
     julia> newton(z -> z^2 + 1, z -> 2*z, -0.1*im, 10)
     0.0 - 1.0im
 ```
 """
-function newton(f::Function, fprime::Function, x0::Number, número_iteraciones::Int = 1000)
+function newton(f::Function, fprime::Function, x0::Number, número_iteraciones::Int = 1000, tolerancia::Real = 1e-8)
     
+    punto_anterior = 0
     punto_actual = float(x0)
     
     for i in 1:número_iteraciones
+        
+        punto_anterior = punto_actual
         punto_actual -= f(punto_actual)/fprime(punto_actual)
     end
     
-    return(punto_actual)
+    diferencia = abs(punto_anterior - punto_actual)
+    
+    if diferencia < tolerancia
+        
+        return(punto_actual)
+        
+    else
+        
+        error("No hay convergencia con tolerancia = $tolerancia. diferencia = $diferencia")
+    end
 end
 
 #Exporta la función:
